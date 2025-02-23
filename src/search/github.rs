8b39454b82c474134168059d22e9a9a0dac7004b
Robin Combrink <robin.combrink@gmail.com>
2025-02-23 02:17:51 +0200

    Begin work on searching through branches

--- src/search/github.rs
+++ src/search/github.rs
@@ -4,6 +4,7 @@ use anyhow::{Context, Result};
 use git_cloner::github::{GitClone, GitCloner};
 use git_cloner::github_authentication::authentication::Authentication;
 use octocrab::models::Repository;
+use tokio::task::JoinSet;
 
 #[derive(Debug)]
 pub struct GithubSearcher<T: Authentication> {
@@ -27,16 +28,53 @@ impl<T: Authentication> GithubSearcher<T> {
     }
 
     pub async fn update_repositories(&self, repo_prefix: &str) -> Result<Vec<Result<()>>> {
-        let repositories = Self::get_repos(&self.owner)
-            .await
-            .with_context(|| "Failed to clone and fetch all repositories")?;
+        let mut tasks: JoinSet<Result<Vec<GitClone>>> = JoinSet::new();
 
-        let git_clones = repositories
-            .iter()
-            .filter(|repo| repo.name.starts_with(repo_prefix))
-            .collect::<Vec<&Repository>>()
+        let branches = Some(vec![
+            "main".to_owned(),
+            "master".to_owned(),
+            "develop".to_owned(),
+        ]);
+
+        let owner = self.owner.clone();
+        Self::get_repos(&owner)
+            .await
+            .with_context(|| "Failed to clone and fetch all repositories")?
             .into_iter()
-            .map(|repo| GitClone::new(self.owner.clone(), repo.name.clone()))
+            .filter(|repo| repo.name.starts_with(repo_prefix))
+            .collect::<Vec<Repository>>()
+            .into_iter()
+            .for_each(|repo| {
+                let name = repo.name;
+                let owner = owner.clone();
+                let branches = branches.clone();
+                tasks.spawn(async move {
+                    let octocrab_instance = octocrab::instance();
+                    let branch_pages = octocrab::instance()
+                        .repos(owner.clone(), name.clone())
+                        .list_branches()
+                        .send()
+                        .await?;
+                    let clones = octocrab_instance
+                        .all_pages(branch_pages)
+                        .await?
+                        .into_iter()
+                        .filter(|branch| match &branches {
+                            Some(branches) => branches.contains(&branch.name),
+                            None => true,
+                        })
+                        .map(|branch| GitClone::new(owner.clone(), name.clone(), branch.name))
+                        .collect();
+                    Ok(clones)
+                });
+            });
+
+        let git_clones = tasks
+            .join_all()
+            .await
+            .into_iter()
+            .filter_map(|git_clone| git_clone.ok())
+            .flatten()
             .collect();
 
         Ok(self.cloner.clone_or_fetch_repositories(git_clones).await)
