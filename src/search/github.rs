bea7d39e5228815edd5b435d3b71bc03901bb02f
Robin Combrink <robin.combrink@gmail.com>
2025-02-24 23:25:14 +0200

    use tokio spawn instead of joinset

--- src/search/github.rs
+++ src/search/github.rs
@@ -4,7 +4,6 @@ use anyhow::{Context, Result};
 use git_cloner::github::{GitClone, GitCloner};
 use git_cloner::github_authentication::authentication::Authentication;
 use octocrab::models::Repository;
-use tokio::task::JoinSet;
 
 #[derive(Debug)]
 pub struct GithubSearcher<T: Authentication> {
@@ -30,50 +29,57 @@ impl<T: Authentication> GithubSearcher<T> {
     }
 
     pub async fn update_repositories(&self, repo_prefix: &str) -> Result<Vec<Result<()>>> {
-        let owner = self.owner.clone();
-        let repositories = Self::get_repos(&owner)
+        let owner = &self.owner;
+        let repositories: Vec<Repository> = Self::get_repos(owner)
             .await
             .with_context(|| "Failed to clone and fetch all repositories")?
             .into_iter()
             .filter(|repo| repo.name.starts_with(repo_prefix))
-            .collect::<Vec<Repository>>();
+            .collect();
 
-        let git_clones: Vec<GitClone> = if self.branches.is_empty() {
+        let git_clones = if self.branches.is_empty() {
             repositories
                 .into_iter()
                 .map(|repo| GitClone::new(owner.clone(), repo.name, None))
-                .collect::<Vec<GitClone>>()
+                .collect()
         } else {
-            let mut tasks: JoinSet<Result<Vec<GitClone>>> = JoinSet::new();
-            repositories.into_iter().for_each(|repo| {
-                let name = repo.name;
-                let owner = owner.clone();
-                let branches = self.branches.clone();
-                tasks.spawn(async move {
-                    let octocrab_instance = octocrab::instance();
-                    let branch_pages = octocrab_instance
-                        .repos(owner.clone(), name.clone())
-                        .list_branches()
-                        .send()
-                        .await?;
-                    let clones = octocrab_instance
-                        .all_pages(branch_pages)
-                        .await?
-                        .into_iter()
-                        .filter(|branch| branches.contains(&branch.name))
-                        .map(|branch| GitClone::new(owner.clone(), name.clone(), Some(branch.name)))
-                        .collect();
-                    Ok(clones)
-                });
-            });
-            tasks
-                .join_all()
-                .await
+            let tasks: Vec<_> = repositories
                 .into_iter()
-                .filter_map(|git_clone| git_clone.ok())
+                .map(|repo| {
+                    let owner = owner.clone();
+                    let branches = self.branches.clone();
+                    let name = repo.name;
+
+                    tokio::spawn(async move {
+                        let octocrab_instance = octocrab::instance();
+                        let branch_pages = octocrab_instance
+                            .repos(&owner, &name)
+                            .list_branches()
+                            .send()
+                            .await?;
+
+                        let clones: Result<Vec<GitClone>> = Ok(octocrab_instance
+                            .all_pages(branch_pages)
+                            .await?
+                            .into_iter()
+                            .filter(|branch| branches.contains(&branch.name))
+                            .map(|branch| {
+                                GitClone::new(owner.clone(), name.clone(), Some(branch.name))
+                            })
+                            .collect());
+                        clones
+                    })
+                })
+                .collect();
+
+            futures::future::try_join_all(tasks)
+                .await?
+                .into_iter()
+                .filter_map(Result::ok)
                 .flatten()
                 .collect()
         };
+
         Ok(self.cloner.clone_or_fetch_repositories(git_clones).await)
     }
 
